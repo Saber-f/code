050ef299d0bf1630f3c8f5dcfc443f969e53a314
Saber-f <735974175@qq.com>
2018-05-16 09:12:28 +0800

    u

--- python_learn/x2y2mn.py
+++ python_learn/x2y2mn.py
@@ -18,8 +18,9 @@ def list2int(l):
 
 def init():
     global n
-    global dec
+    global dec #n列表表示的进度
     global peos_1
+    global l_n # n的dec进制位数
     # 参数设定
     n = int(input("请输入待分解的数::")) # 待分解整数
     dec = 7  # 列表进制
@@ -42,15 +43,22 @@ def init():
         i += 1
     return peos_1
 
-# 适应度函数
+# 适应度函数+排序
 def f(l):
+    rt = [] # 评价结果
+    for l_t in l:
+        n_t1 = list2int(l_t[0:l_n//2])
+        n_t2 = list2int(l_t[l_n//2:l_n])
+        rt.append(abs(n_t2*n_t2 % n - n_t1*n_t1 % n))
+    return rt
 
-
-
-def ga()
+def ga():
+    rt = f(peos_1)
+    print(rt)
 
 def main():
     init() # 初始化
+    ga()
     print(peos_1)
 
 main()
